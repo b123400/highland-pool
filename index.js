7e887007fe7f37c67377ec33323f1ef0f1b532e9
b123400 <b123400@gmail.com>
2016-03-22 19:31:30 +0800

    Added eslint, gitignore, travis

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-'use strict';
+'use strict'
 
-var _ = require('highland');
+var _ = require('highland')
 
-module.exports = function(max) {
-	var streamingCount = 0;
-	var pendingCallbacks = [];
+module.exports = function (max) {
+  var streamingCount = 0
+  var pendingCallbacks = []
 
-	return function(stream) {
-		return _(function(push, next) {
+  return function (stream) {
+    return _(function (push, next) {
+      function startConsume () {
+        streamingCount++
+        stream
+            .on('error', function (err) {
+              push(err)
+              finishConsume()
+            })
+            .each(function (x) {
+              push(null, x)
+            })
+            .done(finishConsume)
+      };
 
-			function startConsume() {
-				streamingCount++;
-				stream
-					.on('error', function(err) {
-						push(err);
-						finishConsume();
-					})
-					.each(function(x) {
-						push(null, x);
-					})
-					.done(finishConsume);
-			}
+      function finishConsume () {
+        push(null, _.nil)
+        streamingCount--
+        if (pendingCallbacks.length) {
+          pendingCallbacks.shift()()
+        }
+      };
 
-			function finishConsume() {
-				push(null, _.nil);
-				streamingCount--;
-				if (pendingCallbacks.length) {
-					pendingCallbacks.shift()();
-				}
-			}
-
-			if (streamingCount < max) {
-				startConsume();
-			} else {
-				pendingCallbacks.push(startConsume);
-			}
-		});
-	}
-};
+      if (streamingCount < max) {
+        startConsume()
+      } else {
+        pendingCallbacks.push(startConsume)
+      }
+    })
+  }
+}
